192d0b1a45017788439765cdcd5b86cc93ff4f62
Ivan Shpak <llutar@inbox.ru>
2022-04-13 22:25:38 +0300

    add lesson-2

---
+++ OOP-Basics/lesson-2/BankAccount.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace lesson_2
+{
+    enum AccountType
+    {
+        DebitAccount, //дебетовый счет
+        CreditAccount,//кредитный счет
+        SavingAccount,//сберегательный счет
+        Unknown       //неизвестный счет
+    }
+
+    class BankAccount
+    {
+        
+        private static int _accountNumber;
+        private decimal _balance;
+        private AccountType _accountType;
+        
+        private static int CreateAccountNumber()
+        {
+            Random rnd = new Random();
+            return _accountNumber = rnd.Next(10000, 99999); 
+        }
+
+        public BankAccount()
+        {
+            CreateAccountNumber();
+            _balance = 0;
+            _accountType = AccountType.Unknown;
+        }
+
+        public BankAccount(decimal _balance)
+        {
+            CreateAccountNumber();
+            this._balance = _balance;
+        }
+
+        public BankAccount(AccountType _accountType)
+        {
+            CreateAccountNumber();
+            this._accountType = _accountType;
+        }
+
+        public BankAccount(decimal _balance, AccountType _accountType)
+        {
+            CreateAccountNumber();
+            this._balance = _balance;
+            this._accountType = _accountType;
+        }
+
+        public void PrintAccount()
+        {
+            Console.WriteLine($"--------------------------\n" +
+                              $"Номер счета: {_accountNumber}\n" +
+                              $"Баланс: {_balance} $\n" +
+                              $"Тип счета: {_accountType}\n" +
+                              $"--------------------------");
+        }
+    }
+}
---
+++ OOP-Basics/lesson-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount1 = new BankAccount();
+            bankAccount1.PrintAccount();
+            bankAccount1 = new BankAccount(5000.00m, AccountType.CreditAccount);
+            bankAccount1.PrintAccount();
+            bankAccount1 = new BankAccount(6895544.87m);
+            bankAccount1.PrintAccount();
+            bankAccount1 = new BankAccount(AccountType.SavingAccount);
+            bankAccount1.PrintAccount();
+
+            Console.ReadKey();
+        }
+    }
+}
